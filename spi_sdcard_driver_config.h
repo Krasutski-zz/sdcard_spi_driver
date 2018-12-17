0766847dda34a2f1536fbbac312f86c0155b9835
Krasutski <Krasutski.Denis@gmail.com>
2018-12-17 03:04:37 +0300

    Add first version

---
+++ spi_sdcard_driver_config.h
@@ -0,0 +1,12 @@
+#ifndef __SPI_SDCARD_DRIVER_CONFIG_H__
+#define __SPI_SDCARD_DRIVER_CONFIG_H__
+
+/* Includes ------------------------------------------------------------------*/
+
+
+#define SPISD_LOG(...)
+
+#define SPISD_ASSERT(cond)  while(!cond){};
+
+
+#endif //__SPI_SDCARD_DRIVER_CONFIG_H__
